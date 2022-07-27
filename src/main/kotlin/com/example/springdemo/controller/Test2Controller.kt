9dda6f7276afb2a63014693e252feec7e1f778b6
fen1xx <artsbrand@gmail.com>
2022-07-27 12:57:28 -0700

    improve

--- src/main/kotlin/com/example/springdemo/controller/Test2Controller.kt
+++ src/main/kotlin/com/example/springdemo/controller/Test2Controller.kt
@@ -1,4 +1,14 @@
 package com.example.springdemo.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/users")
 class Test2Controller {
+
+    @GetMapping
+    fun getUser():String = "User"
+
 }
